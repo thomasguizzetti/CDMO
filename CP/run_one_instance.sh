acecde5edbec5eae5d5a1dadc136f3b777b3b0c0
thomasguizzetti <116814617+thomasguizzetti@users.noreply.github.com>
2023-09-03 21:32:57 +0100

    Add files via upload

--- CP/run_one_instance.sh
+++ CP/run_one_instance.sh
@@ -1,23 +1,27 @@
 #!/bin/bash
 
 # Check if the correct number of arguments is provided
-if [ "$#" -ne 5 ]; then
-    echo "Usage: ./script.sh SOLVER TIME_LIMIT MODEL OUTPUT_FILE INSTANCE_NAME"
+if [ "$#" -ne 4 ]; then
+    echo "Usage: ./script.sh SOLVER MODEL OUTPUT_FILE INSTANCE_NAME"
     exit 1
 fi
 
+TIME_LIMIT=300000
+
 # Capture the arguments
 SOLVER="$1"
-TIME_LIMIT=300000
-MODEL="$3"
-OUTPUT_FILE="$4"
-INSTANCE_NAME="$5"
+MODEL="$2"
+OUTPUT_FILE="$3"
+INSTANCE_NAME="$4"
 
 # Specify the directory containing your instance files
 INSTANCE_DIR="./instance_folder"
+MODEL_DIR="./model_folder"
 
 # Specify the full path to the instance
 INSTANCE_PATH="$INSTANCE_DIR/$INSTANCE_NAME.dzn"
+MODEL_PATH="$MODEL_DIR/$MODEL.mzn"
+
 
 # Empty the output file if it exists
 > "$OUTPUT_FILE"
@@ -30,10 +34,10 @@ echo "Running Instance: $instance_name_no_ext" >> "$OUTPUT_FILE"
 
 # Print the command being executed
 echo "Running command:"
-echo "minizinc --solver $SOLVER --time-limit $TIME_LIMIT --output-time $MODEL.mzn $INSTANCE_PATH"
+echo "minizinc --solver $SOLVER --time-limit $TIME_LIMIT --output-time $MODEL_PATH $INSTANCE_PATH"
 
 # Run the minizinc command for the specified instance and save the output to the txt file
 minizinc --solver $SOLVER --time-limit $TIME_LIMIT \
-         --output-time $MODEL.mzn $INSTANCE_PATH >> "$OUTPUT_FILE"
+         --output-time $MODEL_PATH $INSTANCE_PATH >> "$OUTPUT_FILE"
 
 echo "" >> "$OUTPUT_FILE"
